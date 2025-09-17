7b3a9499afc4fe67cdd6f4d1cccb73468ef16468
Andres <andrsbtrg@gmail.com>
2025-09-17 21:41:47 +0200

    use nmtui for wifi

--- .config/waybar/wifi.sh
+++ .config/waybar/wifi.sh
@@ -1,4 +1,5 @@
-wn=$(nmcli dev wifi list | fzf)
-ssid="$(echo $wn | awk '{print $2}')"
-read -e -p "Password: " pw
-nmcli dev wifi connect "$ssid" password $pw
+# wn=$(nmcli dev wifi list | fzf)
+# ssid="$(echo $wn | awk '{print $2}')"
+# read -e -p "Password: " pw
+# nmcli dev wifi connect "$ssid" password $pw
+nmtui
